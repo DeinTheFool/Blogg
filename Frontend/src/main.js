b863b529f2dc12632325a757066e24f53cf8b3a3
DeinTheFool <delia002@osloskolen.no>
2026-01-27 14:34:58 +0100

    Author test
    Test for å sjekke om author virker

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,9 +1,12 @@
 // rask js for å laste opp testsidenes funksjoner
 import { renderPostForm } from './components/post-form.js'
 import { renderPostList } from './components/post-list.js'
+import { createClient } from '@supabase/supabase-js'
 
 const formContainer = document.getElementById('post-form')
 const listContainer = document.getElementById('post-list')
+
+
 
 renderPostForm(formContainer, async () => {
   await renderPostList(listContainer)
